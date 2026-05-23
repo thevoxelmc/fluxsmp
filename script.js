c6752dbe945d5886d8ba2754f8f83a260f37e941
ThyIr0n <jatinmaddu.dev@gmail.com>
2026-05-23 00:46:03 -0400

    Change server IP and notify on copy
    Updated the server IP and added an alert after copying.

--- script.js
+++ script.js
@@ -1,48 +1,8 @@
 // script.js
 
 function copyIP(){
-    navigator.clipboard.writeText("play.fluxsmp.com");
 
-    const btns = document.querySelectorAll(".primary-btn");
+    navigator.clipboard.writeText("play.fluxsmp.net");
 
-    btns.forEach(btn=>{
-        const original = btn.innerHTML;
-
-        btn.innerHTML = "Copied!";
-        
-        setTimeout(()=>{
-            btn.innerHTML = original;
-        },1500);
-    });
+    alert("Server IP copied!");
 }
-
-const glow = document.querySelector(".mouse-glow");
-
-document.addEventListener("mousemove",(e)=>{
-
-    glow.style.left = e.clientX + "px";
-    glow.style.top = e.clientY + "px";
-
-});
-
-const reveals = document.querySelectorAll(".reveal");
-
-function revealScroll(){
-
-    const trigger = window.innerHeight * 0.85;
-
-    reveals.forEach(el=>{
-
-        const top = el.getBoundingClientRect().top;
-
-        if(top < trigger){
-            el.classList.add("active");
-        }
-
-    });
-
-}
-
-window.addEventListener("scroll", revealScroll);
-
-revealScroll();
